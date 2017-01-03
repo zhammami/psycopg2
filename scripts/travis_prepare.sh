336c42dfc23b34e77e933b722a0bee5ce8373524
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-01-03 18:16:36 +0100

    Use the travis matrix to test different postgres versions

--- scripts/travis_prepare.sh
+++ scripts/travis_prepare.sh
@@ -9,52 +9,41 @@ set -e
 
 set_param () {
     # Set a parameter in a postgresql.conf file
-    version=$1
-    param=$2
-    value=$3
+    param=$1
+    value=$2
 
     sed -i "s/^\s*#\?\s*$param.*/$param = $value/" \
-        "/etc/postgresql/$version/psycopg/postgresql.conf"
+        "/etc/postgresql/$PGVERSION/psycopg/postgresql.conf"
 }
 
-create () {
-    version=$1
-    port=$2
-    dbname=psycopg2_test
-
-    pg_createcluster -p $port --start-conf manual $version psycopg
-
-    # for two-phase commit testing
-    set_param "$version" max_prepared_transactions 10
-
-    # for replication testing
-    set_param "$version" max_wal_senders 5
-    set_param "$version" max_replication_slots 5
-    if [ "$version" == "9.2" -o "$version" == "9.3" ]
-    then
-        set_param "$version" wal_level hot_standby
-    else
-        set_param "$version" wal_level logical
-    fi
-
-    echo "local replication travis trust" \
-        >> "/etc/postgresql/$version/psycopg/pg_hba.conf"
-
-
-    pg_ctlcluster "$version" psycopg start
-
-    sudo -u postgres psql -c "create user travis replication" "port=$port"
-    sudo -u postgres psql -c "create database $dbname" "port=$port"
-    sudo -u postgres psql -c "grant create on database $dbname to travis" "port=$port"
-    sudo -u postgres psql -c "create extension hstore" "port=$port dbname=$dbname"
-}
-
+dbname=psycopg2_test
+dbport=15432
 
 # Would give a permission denied error in the travis build dir
 cd /
 
-create 9.6 54396
-create 9.5 54395
-create 9.4 54394
-create 9.3 54393
-create 9.2 54392
+pg_createcluster -p $dbport --start-conf manual $PGVERSION psycopg
+
+# for two-phase commit testing
+set_param max_prepared_transactions 10
+
+# for replication testing
+set_param max_wal_senders 5
+set_param max_replication_slots 5
+if [ "$PGVERSION" == "9.2" -o "$PGVERSION" == "9.3" ]
+then
+    set_param wal_level hot_standby
+else
+    set_param wal_level logical
+fi
+
+echo "local replication travis trust" \
+    >> "/etc/postgresql/$PGVERSION/psycopg/pg_hba.conf"
+
+
+pg_ctlcluster "$PGVERSION" psycopg start
+
+sudo -u postgres psql -c "create user travis replication" "port=$dbport"
+sudo -u postgres psql -c "create database $dbname" "port=$dbport"
+sudo -u postgres psql -c "grant create on database $dbname to travis" "port=$dbport"
+sudo -u postgres psql -c "create extension hstore" "port=$dbport dbname=$dbname"
--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -5,26 +5,17 @@
 
 set -e
 
-run_test () {
-    version=$1
-    port=$2
-    dbname=psycopg2_test
+dbname=psycopg2_test
+dbport=15432
 
-    printf "\n\nRunning tests against PostgreSQL $version\n\n"
-    export PSYCOPG2_TESTDB=$dbname
-    export PSYCOPG2_TESTDB_PORT=$port
-    export PSYCOPG2_TESTDB_USER=travis
-    export PSYCOPG2_TEST_REPL_DSN=
-    unset PSYCOPG2_TEST_GREEN
-    python -c "from psycopg2 import tests; tests.unittest.main(defaultTest='tests.test_suite')"
+printf "\n\nRunning tests against PostgreSQL $PGVERSION\n\n"
+export PSYCOPG2_TESTDB=$dbname
+export PSYCOPG2_TESTDB_PORT=$dbport
+export PSYCOPG2_TESTDB_USER=travis
+export PSYCOPG2_TEST_REPL_DSN=
+unset PSYCOPG2_TEST_GREEN
+python -c "from psycopg2 import tests; tests.unittest.main(defaultTest='tests.test_suite')"
 
-    printf "\n\nRunning tests against PostgreSQL $version (green mode)\n\n"
-    export PSYCOPG2_TEST_GREEN=1
-    python -c "from psycopg2 import tests; tests.unittest.main(defaultTest='tests.test_suite')"
-}
-
-run_test 9.6 54396
-run_test 9.5 54395
-run_test 9.4 54394
-run_test 9.3 54393
-run_test 9.2 54392
+printf "\n\nRunning tests against PostgreSQL $PGVERSION (green mode)\n\n"
+export PSYCOPG2_TEST_GREEN=1
+python -c "from psycopg2 import tests; tests.unittest.main(defaultTest='tests.test_suite')"
